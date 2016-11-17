02636973b76e9df7eeae78060774f05568cbf4b7
unknown <hanzichi@outlook.com>
2016-11-17 20:32:51 +0800

    代码规范

--- minimumRepresentation.cpp
+++ minimumRepresentation.cpp
@@ -1,17 +1,23 @@
-int Min_pos(char *s,int len)
-{
-	int i=0,j=1,k=0;
-	while(i<len&&j<len&&k<len)
-	{
-		int t=s[(i+k)%len]-s[(j+k)%len];
-		if(!t) k++;
-		else
-		{
-			if(t>0) i+=k+1;     //最大表示改这里的符号
-			else j+=k+1;
-			if(i==j) j++;
+int Min_pos(char *s, int len) {
+	int i = 0, j = 1, k = 0;
+
+	while (i < len && j < len && k < len) {
+		int t = s[(i + k) % len] - s[(j + k) % len];
+
+		if (!t)
+			k++;
+		else {
+			if (t > 0)
+				i += k + 1;  // 最大表示改这里的符号
+			else
+				j += k + 1;
+
+			if (i == j)
+				j++;
+
 			k=0;
 		}
 	}
-	return i<j?i:j;
+
+	return i < j ? i : j;
 }
